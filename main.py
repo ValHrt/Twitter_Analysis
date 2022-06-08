5ed06e724189a3ae7753ae80047ee6e06c416cc2
Valentin Henriot <59604857+ValHrt@users.noreply.github.com>
2022-06-08 14:45:05 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+import tweepy
+import statistics
+
+# Authentification auprès de l'API Twitter
+print(os.environ.get("CONSUMER_KEY"))
+
+#auth = tweepy.OAuthHandler(os.environ.get("CONSUMER_KEY"),
+#                           os.environ.get("CONSUMER_SECRET"))
+#
+#auth.set_access_token(os.environ.get("TOKEN_KEY"),
+#                      os.environ.get("TOKEN_SECRET"))
+
