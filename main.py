0707d6efef807fab0fbeb85ecc3c9365ac89b23d
Valentin Henriot <59604857+ValHrt@users.noreply.github.com>
2022-06-08 14:50:38 +0200

    Delete print statement for debugging

--- main.py
+++ main.py
@@ -3,11 +3,10 @@ import tweepy
 import statistics
 
 # Authentification auprès de l'API Twitter
-print(os.environ.get("CONSUMER_KEY"))
 
-#auth = tweepy.OAuthHandler(os.environ.get("CONSUMER_KEY"),
-#                           os.environ.get("CONSUMER_SECRET"))
-#
-#auth.set_access_token(os.environ.get("TOKEN_KEY"),
-#                      os.environ.get("TOKEN_SECRET"))
+auth = tweepy.OAuthHandler(os.environ.get("CONSUMER_KEY"),
+                           os.environ.get("CONSUMER_SECRET"))
+
+auth.set_access_token(os.environ.get("TOKEN_KEY"),
+                      os.environ.get("TOKEN_SECRET"))
 
