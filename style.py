1c283a90f7b301f06d222c0a1dc943d9efc6e2e7
Valentin Henriot <59604857+ValHrt@users.noreply.github.com>
2022-08-15 18:57:31 +0200

    Add style for second middle box

--- style.py
+++ style.py
@@ -20,6 +20,17 @@ def BoxStyleMiddle():
         }
     """
 
+def BoxStyleMiddle_2():
+    return """
+        QGroupBox{
+        background-color:#7858A6;
+        font:20pt Cochin;
+        color:white;
+        border:2px solid gray;
+        border-radius:15px;
+        }
+    """
+
 def RadioButtonCompare():
     return """
         QRadioButton{
